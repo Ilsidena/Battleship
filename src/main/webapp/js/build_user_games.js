16c6ee135601a69d8070ab2f032d41f4f0d93397
Ilsidena <ilsidena@mail.ru>
2019-01-30 20:52:08 +0300

    Work with user's games interface js

--- src/main/webapp/js/build_user_games.js
+++ src/main/webapp/js/build_user_games.js
@@ -1,4 +1,4 @@
-var currentGame = 0;
+var enemy = 0;
 
 function build_block1(){
     $.ajax({
@@ -65,18 +65,6 @@ function build_block1(){
                                             }
                                         }
                                         else if (vis == "hidden") {
-//                                            var list = $("DIV");
-//                                            var length = list.length;
-//                                            for (var k = 0; k < length; ++k){
-//                                               if ($(list[k]).hasClass("sb-element")){
-//                                                   $(list[k]).removeClass("sb-our-ship sb-miss sb-hit sb-enemy-ship");
-//                                                   console.log(list[k].className);
-//                                               }
-//                                            }
-//                                            list.html();
-
-                                            $(arrayToClick[i]).removeClass("sb-our-ship sb-miss sb-hit sb-enemy-ship");
-
                                             $(".game").css("visibility", "visible");
                                             this["wasClicked"] = 1;
 
@@ -91,7 +79,7 @@ function build_block1(){
     });
 }
 
-function build_table(field){
+function build_table(field, shiftN){
     field.addClass("table")
                 .append(
                     $("<tr></tr>")
@@ -110,49 +98,85 @@ function build_table(field){
                 );
 
     for (var i = 0; i < 10; ++i) {
-        field.append(
-            $("<tr></tr>")
-                .addClass("table-row")
-                .append($("<th></th>").html(i + 1))
-                .append($("<th></th>").html("<div class=\"sb-element\"></div>"))
-                .append($("<th></th>").html("<div class=\"sb-element\"></div>"))
-                .append($("<th></th>").html("<div class=\"sb-element\"></div>"))
-                .append($("<th></th>").html("<div class=\"sb-element\"></div>"))
-                .append($("<th></th>").html("<div class=\"sb-element\"></div>"))
-                .append($("<th></th>").html("<div class=\"sb-element\"></div>"))
-                .append($("<th></th>").html("<div class=\"sb-element\"></div>"))
-                .append($("<th></th>").html("<div class=\"sb-element\"></div>"))
-                .append($("<th></th>").html("<div class=\"sb-element\"></div>"))
-                .append($("<th></th>").html("<div class=\"sb-element\"></div>"))
-        );
+        var tr = $("<tr></tr>")
+                    .addClass("table-row")
+                    .append($("<th></th>").html(i + 1));
+        for(var j = 0; j < 10; ++j){
+            var div = $("<div></div>").addClass("sb-element");
+            div.attr("my", i * 10 + j + shiftN);
+            tr.append(
+                $("<td></td>").append(div)
+            );
+        }
+        field.append(tr);
     }
     return field;
 }
 
 function get_game(gameID){
+    if(typeof gameID !== "undefined")
+        this.gameID = gameID;
+
+    if(typeof this.gameID === "undefined")
+        throw new Error("this.gameID is undefined");
+
+    var that = this;
+
     $.ajax({
         dataType: "json",
-        url: "http://localhost:8080/Battleship/get_game?id=" + gameID,
+        url: "http://localhost:8080/Battleship/get_game?id=" + this.gameID,
         success: function (data) {
-                     var field = data.field;
-
-                     var list = $("DIV");
-                     var length = list.length;
                      var i = 0;
+                     var field = data.field;
+                      var move = data.move;
+                      var status = data.status;
 
-//                     if (window.currentGame != gameID){
-//                        for (var k = 0; k < length; ++k){
-//                            if ($(list[k]).hasClass("sb-element")){
-//                                $(list[k]).removeClass("sb-our-ship sb-miss sb-hit sb-enemy-ship");
-//                                console.log($(list[k]).hasClass("sb-our-ship sb-miss sb-hit sb-enemy-ship"));
-//                            }
-//                        }
-//                     }
-//
-//                     window.currentGame = gameID;
+                      if (status > 0) {
+                         if (move == 0) {
+                             alert("YOU ARE THE WINNER!!!");
+                         }
+                         else {
+                             alert("Sorry, you lost...");
+                         }
+                         return;
+                      }
+
+                      if (move == 1){
+                         $("#player_move").html("you");
+                      }
+                      else {
+                         $("#player_move").html("enemy");
+                      }
+                      console.log(move);
+                      var list = $("div.sb-element");
+                      var length = list.length;
+                      var i = 0;
+
+                      list.removeClass("sb-our-ship sb-miss sb-hit sb-enemy-ship");
 
                      for (var j = 0; j < length; ++j){
-                         if ($(list[j]).hasClass("sb-element")){
+                         //if ($(list[j]).hasClass("sb-element")){
+                             //list[j]["my"] = i / 2;
+                             list[j].onclick = function(){
+                                if (move == 1){
+                                   if (this.getAttribute("my") >= 100) {
+                                      if (field[2 * this.getAttribute("my") + 1] == 0) {
+                                         field[2 * this.getAttribute("my") + 1] = 1;
+
+                                         $.ajax({
+                                             dataType: "json",
+                                             url: "http://localhost:8080/Battleship/move?gameID=" + that.gameID + "&cell=" + this.getAttribute("my")
+                                         });
+                                         $("#player_move").html("enemy");
+
+
+                                      }
+                                      else {
+                                         alert("You can not do it twice!");
+                                      }
+                                   }
+                                }
+                             };
                              if (field[i] == 1){
                                  if (i < 200){
                                      $(list[j]).addClass("sb-our-ship");
@@ -172,9 +196,9 @@ function get_game(gameID){
                                  }
                              }
                              i++;
-                         }
+                         //}
                      }
-                     setTimeout(get_game(gameID), 2000);
+                     setTimeout(get_game, 2000);
                  }
     });
 }
@@ -188,16 +212,19 @@ var newGameForm = $("<div></div>").html("<form  action=\"new_game\" method=\"pos
 $("#row1").append(newGameForm);
 
 var game = $("<div></div>").addClass("game");
+var gameStatus = $("<h3></h3>").html("---").attr("id", "player_move");
+game.append(gameStatus);
 build_block1();
 
 var field1 = $("<table></table>");
 var field2 = $("<table></table>");
 var div1 = $("<div></div>").addClass("field")
     .append($("<h3></h3>").html("Your field"))
-    .append(build_table(field1));
+    .append(build_table(field1, 0));
 var div2 = $("<div></div>").addClass("field")
     .append($("<h3></h3>").html("Enemy's field"))
-    .append(build_table(field2));
+    .append(build_table(field2, 100));
+//var infobox = $("<div></div>").addClass("infobox").html("Enemy: ");
 game.append(div1).append(div2);
 $("#block2").append(game);
 
